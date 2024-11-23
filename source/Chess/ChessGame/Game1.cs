509d39c3dbe88bdb254f0e85c02218e18d1bebd9
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2024-11-23 16:47:08 +0100

    Adds texture during startup
    missing textures for now

--- source/Chess/ChessGame/Game1.cs
+++ source/Chess/ChessGame/Game1.cs
@@ -12,18 +12,25 @@ public class Game1 : Game
     private int _playerTurn;
     
     // Chessboard 8x8
-    private Piece[,] ChessBoard = {
-        {new Piece("Rook", 1), new Piece("Knight", 1), new Piece("Bishop", 1), new Piece("King", 1), new Piece("Queen", 1), new Piece("Bishop", 1), new Piece("Knight", 1), new Piece("Rook", 1)}, 
-        {new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1), new Piece("Pawn", 1)}, 
-        {new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2)}, 
-        {new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2)}, 
-        {new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2)},
-        {new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2), new Piece("", 2)}, 
-        {new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0), new Piece("Pawn", 0)},
-        {new Piece("Rook", 0), new Piece("Knight", 0), new Piece("Bishop", 0), new Piece("King", 0), new Piece("Queen", 0), new Piece("Bishop", 0), new Piece("Knight", 0), new Piece("Rook", 0)}
-    };
+    private Piece[,] ChessBoard;
     
     private KeyboardState _currentKeyboard;
+
+    private Texture2D _kingWhiteTexture;
+    private Texture2D _queenWhiteTexture;
+    private Texture2D _bishopWhiteTexture;
+    private Texture2D _knightWhiteTexture;
+    private Texture2D _rookWhiteTexture;
+    private Texture2D _pawnWhiteTexture;
+    
+    private Texture2D _kingBlackTexture;
+    private Texture2D _queenBlackTexture;
+    private Texture2D _bishopBlackTexture;
+    private Texture2D _knightBlackTexture;
+    private Texture2D _rookBlackTexture;
+    private Texture2D _pawnBlackTexture;
+
+    private Texture2D _emptyTexture;
     
     public Game1()
     {
@@ -47,6 +54,112 @@ public class Game1 : Game
 
     protected override void LoadContent()
     {
+        _kingWhiteTexture = Content.Load<Texture2D>("kingWhite2d");
+        _queenWhiteTexture = Content.Load<Texture2D>("queenWhite2d");
+        _bishopWhiteTexture = Content.Load<Texture2D>("bishopWhite2d");
+        _knightWhiteTexture = Content.Load<Texture2D>("knightWhite2d");
+        _rookWhiteTexture = Content.Load<Texture2D>("rookWhite2d");
+        _pawnWhiteTexture = Content.Load<Texture2D>("pawnWhite2d");
+        
+        _kingBlackTexture = Content.Load<Texture2D>("kingBlack2d");
+        _queenBlackTexture = Content.Load<Texture2D>("queenBlack2d");
+        _bishopBlackTexture = Content.Load<Texture2D>("bishopBlack2d");
+        _knightBlackTexture = Content.Load<Texture2D>("knightBlack2d");
+        _rookBlackTexture = Content.Load<Texture2D>("rookBlack2d");
+        _pawnBlackTexture = Content.Load<Texture2D>("pawnBlack2d");
+
+        _emptyTexture = new Texture2D(GraphicsDevice, 1, 1);
+        
+        ChessBoard = new Piece[,] {
+            {
+                new Piece("Rook", 1, new Vector2(50, 50), _pawnBlackTexture), 
+                new Piece("Knight", 1, new Vector2(150, 50), _knightBlackTexture), 
+                new Piece("Bishop", 1, new Vector2(250, 50), _bishopBlackTexture), 
+                new Piece("King", 1, new Vector2(350, 50), _kingBlackTexture), 
+                new Piece("Queen", 1, new Vector2(450, 50), _queenBlackTexture), 
+                new Piece("Bishop", 1, new Vector2(550, 50), _bishopBlackTexture), 
+                new Piece("Knight", 1, new Vector2(650, 50), _knightBlackTexture), 
+                new Piece("Rook", 1, new Vector2(750, 50), _rookBlackTexture)
+            },
+
+            {
+                new Piece("Pawn", 1, new Vector2(50, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(150, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(250, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(350, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(450, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(550, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(650, 150), _pawnBlackTexture), 
+                new Piece("Pawn", 1, new Vector2(750, 150), _pawnBlackTexture)
+            },
+
+            {
+                new Piece("", 2, new Vector2(50, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(150, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(250, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(350, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(450, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(550, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(650, 250), _emptyTexture), 
+                new Piece("", 2, new Vector2(750, 250), _emptyTexture)
+            },
+
+            {
+                new Piece("", 2, new Vector2(50,350), _emptyTexture), 
+                new Piece("", 2, new Vector2(150, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(250, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(350, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(450, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(550, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(650, 350), _emptyTexture), 
+                new Piece("", 2, new Vector2(750, 350), _emptyTexture)
+            },
+
+            {
+                new Piece("", 2, new Vector2(50, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(150, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(250, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(350, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(450, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(550, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(650, 450), _emptyTexture), 
+                new Piece("", 2, new Vector2(750, 450), _emptyTexture)
+            },
+
+            {
+                new Piece("", 2, new Vector2(50, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(150, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(250, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(350, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(450, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(550, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(650, 550), _emptyTexture), 
+                new Piece("", 2, new Vector2(750, 550), _emptyTexture)
+            },
+
+            {
+                new Piece("Pawn", 0, new Vector2(50, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(150, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(250, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(350, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(450, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(550, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(650, 650), _pawnWhiteTexture), 
+                new Piece("Pawn", 0, new Vector2(750, 650), _pawnWhiteTexture)
+            },
+
+            {
+                new Piece("Rook", 0, new Vector2(50, 750), _rookWhiteTexture), 
+                new Piece("Knight", 0, new Vector2(150, 750), _knightWhiteTexture), 
+                new Piece("Bishop", 0, new Vector2(250, 750), _bishopWhiteTexture), 
+                new Piece("King", 0, new Vector2(350, 750), _kingWhiteTexture), 
+                new Piece("Queen", 0, new Vector2(450, 750), _queenWhiteTexture), 
+                new Piece("Bishop", 0, new Vector2(550, 750), _bishopWhiteTexture), 
+                new Piece("Knight", 0, new Vector2(650, 750), _knightWhiteTexture), 
+                new Piece("Rook", 0, new Vector2(750, 750), _rookWhiteTexture)
+            }
+        };
+        
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         
         // TODO: use this.Content to load your game content here
@@ -70,6 +183,20 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         _spriteBatch.Begin();
+
+        int x = 0; int y = 0;
+        
+        for (int i = 7; i >= 0; i--)    // i = řádek
+        {
+            for (int ii = 7; ii >= 0; ii--)  // ii = sloupec
+            {
+                ChessBoard[i,ii].DrawSelf(_spriteBatch);
+                x += 100;
+            }
+
+            y += 100;
+        }
+        
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- source/Chess/ChessGame/Piece.cs
+++ source/Chess/ChessGame/Piece.cs
@@ -1,24 +1,42 @@
-﻿namespace ChessGame;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ChessGame;
 
 public class Piece
 {
     private string _type;
     private int _team;
+
+    private Texture2D _texture;
+    private Vector2 _vector2;
     
-    public Piece(string type, int team)
+    public Piece(string type, int team, Vector2 vector2, Texture2D texture)
     {
         _type = type;
         _team = team;
+        _vector2 = vector2;
+        _texture = texture;
     }
-
-    public void DrawSelf(int X, int Y)
+    
+    public void DrawSelf(SpriteBatch spriteBatch)
     {
-        
+        spriteBatch.Draw(_texture, _vector2, Color.White);
     }
 
     public bool IsEmpty()
     {
-        if (_team == 2){ return false; }
+        if (_team == 2) { return false; }
         else return true;
+    }
+
+    public void replacePiece(Piece toReplacePiece)
+    {
+        _vector2 = toReplacePiece.getVector2();
+    }
+
+    public Vector2 getVector2()
+    {
+        return _vector2;
     }
 }
